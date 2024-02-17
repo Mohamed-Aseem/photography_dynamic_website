e4393d63ee53873064eed9526f5572e8ca44add2
Mohamed-Aseem <ima7str@gmail.com>
2024-02-17 16:52:42 +0530

    Created About Page

---
+++ about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include "header.php";
+    ?>
+</head>
+<body>
+    <nav>
+    <?php
+        include "nav.php";
+    ?>
+    </nav>
+    
+
+    <div class="container about_container">
+        <div class="about_left">
+            <h2 class="about_title">About <span>@Malcolm</span></h2>
+            <div class="about_image">
+                <img src="imgs/About.png" alt="Malcolm Lismore">
+            </div>
+        </div>
+        <Div class="about_right">
+            <div class="about_head">
+                <div class="empty about_empty"></div>
+                <a class="about_tag">#Malcolm_Lismore</a>
+            </div>
+            <h3>Hello, I'm Malcolm Lismore, a passionate and seasoned freelance photographer nestled in the serene landscapes of the North West coast of Scotland</h3>
+            <p>
+            With years of experience capturing the breathtaking beauty of Scotland's rugged landscapes, its diverse wildlife, and captivating coastal scenery, my photography journey began as an exploration of my profound love for the natural world. Over time, this passion has led me to weave stories through my lens, whether it's the untamed allure of Scottish wildlife or the timeless charm of weddings and special events.
+
+            <br/><br/>Every click of the shutter is aimed at encapsulating those moments of excitement, joy, and sheer happiness, turning them into timeless memories. I've had the privilege to work across various spectrums of photography – from weddings and portraits to capturing the essence of special events. My pursuit is to craft the perfect picture for every significant moment, and that's what drives me every single day.
+            </p>
+            <div class="primary_btn about_btn">
+                <a href="gallery.php">See My Works</a>
+            </div>
+        </Div>
+    </div>
+    <script src="main.js"></script>
+</body>
+</html>
