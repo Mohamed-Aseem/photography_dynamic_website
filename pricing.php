e6cde602e4741b4105e5e6f433ffb2816615e9cb
Mohamed-Aseem <ima7str@gmail.com>
2024-02-19 18:00:19 +0530

    Added pricing page

---
+++ pricing.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include "header.php";
+    ?>
+</head>
+<body>
+    <nav>
+        <?php
+            include "nav.php";
+        ?>
+    </nav>
+
+    <div class="container price_container">
+        <h2>Pricing / Services</h2>
+        <div class="prices">
+            <div class="price_image">
+                <img src="imgs/potraits_4.png" alt="Wedding">
+            </div>
+            <div class="contents">
+                <h3>Portrait Session</h3>
+                <p>
+                    <b>Starts at - $250</b><br>
+                    This covers Family portraits, Professional or Personal headshots, Senior Photos, and Non-Newborn shoots. Up to 4 people are allowed, $10 additional per person after 4. You get 20-30 edited images approximately.
+                </p>
+            </div>
+        </div>
+        <div class="prices">
+            <div class="contents">
+                <h3>Weddings</h3>
+                <p>
+                    <b>Starts at - $450</b><br>
+                    Wedding photography services are available at $450 for a four-hour session. This package encompasses expert coverage of your special day, capturing the essence of your wedding ceremony, candid moments, and celebratory ambiance. Clients receive beautifully edited high-resolution images within one day, encapsulating the joy and love of the occasion.
+                </p>
+            </div>
+            <div class="price_image">
+                <img src="imgs/potraits_6-1.png" alt="Wedding">
+            </div>
+        </div>
+        <div class="prices">
+            <div class="price_image">
+                <img src="imgs/potraits_8-1.png" alt="Wedding">
+            </div>
+            <div class="contents">
+                <h3>Specialty Session</h3>
+                <p>
+                    <b>Starts at - $450</b><br>
+                    Custom work is anything from boudoir, sports events, speaking events, costume/cosplay, etc. The maximum shooting time will not exceed 3 hours, and price is set according to project details.
+                </p>
+            </div>
+        </div>
+        <div class="prices">
+            <div class="contents">
+                <h3>Maternity / Baby Portraits</h3>
+                <p>
+                    <b>Starts at - $150</b><br>
+                    Capture the precious moments of your little one's earliest stages with our newborn to 6-month-old photography sessions, starting at $150 for approximately 2 hours. This package includes 20-30 meticulously edited images from a 2-hour session, focusing on up to 3 poses to cherish this tender time. Additionally, our maternity photo shoots are available at $170, offering 30-40 beautifully edited images from sessions lasting between 1.5 to 2 hours.
+                </p>
+            </div>
+            <div class="price_image">
+                <img src="imgs/potraits_11-1.png" alt="Wedding">
+            </div>
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+    <script src="js/main.js"></script><script src="main.js"></script>
+</body>
+</html>
