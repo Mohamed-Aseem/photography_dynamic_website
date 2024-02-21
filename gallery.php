aa9ea077a507081163086ff777c0645794925104
Mohamed-Aseem <ima7str@gmail.com>
2024-02-21 12:03:20 +0530

    Added gallery page

---
+++ gallery.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include "header.php";
+    ?>
+</head>
+<body>
+    <nav>
+        <?php
+            include "nav.php";
+        ?>
+    </nav>
+
+    <div class="albums">
+        <div class="container album_container">
+            <article class="album">
+                <div class="album_thumbnail">
+                    <img src="imgs/wildlife-2.png" alt="Wild Life">
+                </div>
+                <div class="album_info">
+                    <a href="gallery_wild-life.php" class="category_button">Wild Life</a>
+                    <h4 class="album_details">
+                        <a href="gallery_wild-life.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                    </h4>
+                </div>
+            </article>
+            <article class="album">
+                <div class="album_thumbnail">
+                    <img src="imgs/landscape/landscape_01-1.jpg" alt="Landscape">
+                </div>
+                <div class="album_info">
+                    <a href="gallery_landscape.php" class="category_button">Landscape</a>
+                    <h4 class="album_details">
+                        <a href="gallery_landscape.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                    </h4>
+                </div>
+            </article>
+            <article class="album">
+                <div class="album_thumbnail">
+                    <img src="imgs/coastal_bird/bird_02.jpg" alt="Coastal Birds">
+                </div>
+                <div class="album_info">
+                    <a href="gallery_coastal_birds.php" class="category_button">Coastal Birds</a>
+                    <h4 class="album_details">
+                        <a href="gallery_coastal_birds.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                    </h4>
+                </div>
+            </article>
+            <article class="album">
+                <div class="album_thumbnail">
+                    <img src="imgs/wedding/wedding_04-1.jpg" alt="Wedding">
+                </div>
+                <div class="album_info">
+                    <a href="gallery_wedding.php" class="category_button">Weddings</a>
+                    <h4 class="album_details">
+                        <a href="gallery_wedding.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                    </h4>
+                </div>
+            </article>
+            <article class="album">
+                <div class="album_thumbnail">
+                    <img src="imgs/portraits/port_03-1.jpg" alt="Portraits">
+                </div>
+                <div class="album_info">
+                    <a href="gallery_portraits.php" class="category_button">Portraits</a>
+                    <h4 class="album_details">
+                        <a href="gallery_portraits.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                    </h4>
+                </div>
+            </article>
+            <article class="album">
+                <div class="album_thumbnail">
+                    <img src="imgs/special_event/sp_event_05-1.jpg" alt="Special Events">
+                </div>
+                <div class="album_info">
+                    <a href="gallery_special_event.php" class="category_button">Special Event</a>
+                    <h4 class="album_details">
+                        <a href="gallery_special_event.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                    </h4>
+                </div>
+            </article>
+            <article class="album">
+                <div class="album_thumbnail">
+                    <img src="imgs/maternity/baby_port_02-1.jpg" alt="Wild Life">
+                </div>
+                <div class="album_info">
+                    <a href="gallery_maternity.php" class="category_button">Baby Portraits</a>
+                    <h4 class="album_details">
+                        <a href="gallery_maternity.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                    </h4>
+                </div>
+            </article>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="main.js"></script><script src="main.js"></script>
+</body>
+</html>
