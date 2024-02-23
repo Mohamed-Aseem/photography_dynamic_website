f38e9d3f2b44171d681628e3777464dc7caadb39
Mohamed-Aseem <156054223+Mohamed-Aseem@users.noreply.github.com>
2024-02-23 21:35:03 +0530

    Update gallery.php with locations of gallery related pages

--- gallery.php
+++ gallery.php
@@ -19,9 +19,9 @@
                     <img src="imgs/wildlife-2.png" alt="Wild Life">
                 </div>
                 <div class="album_info">
-                    <a href="gallery_wild-life.php" class="category_button">Wild Life</a>
+                    <a href="gallery/gallery_wild-life.php" class="category_button">Wild Life</a>
                     <h4 class="album_details">
-                        <a href="gallery_wild-life.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                        <a href="gallery/gallery_wild-life.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
                     </h4>
                 </div>
             </article>
@@ -30,9 +30,9 @@
                     <img src="imgs/landscape_01-1.jpg" alt="Landscape">
                 </div>
                 <div class="album_info">
-                    <a href="gallery_landscape.php" class="category_button">Landscape</a>
+                    <a href="gallery/gallery_landscape.php" class="category_button">Landscape</a>
                     <h4 class="album_details">
-                        <a href="gallery_landscape.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                        <a href="gallery/gallery_landscape.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
                     </h4>
                 </div>
             </article>
@@ -41,9 +41,9 @@
                     <img src="imgs/bird_02.jpg" alt="Coastal Birds">
                 </div>
                 <div class="album_info">
-                    <a href="gallery_coastal_birds.php" class="category_button">Coastal Birds</a>
+                    <a href="gallery/gallery_coastal_birds.php" class="category_button">Coastal Birds</a>
                     <h4 class="album_details">
-                        <a href="gallery_coastal_birds.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                        <a href="gallery/gallery_coastal_birds.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
                     </h4>
                 </div>
             </article>
@@ -52,9 +52,9 @@
                     <img src="imgs/wedding_04-1.jpg" alt="Wedding">
                 </div>
                 <div class="album_info">
-                    <a href="gallery_wedding.php" class="category_button">Weddings</a>
+                    <a href="gallery/gallery_wedding.php" class="category_button">Weddings</a>
                     <h4 class="album_details">
-                        <a href="gallery_wedding.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                        <a href="gallery/gallery_wedding.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
                     </h4>
                 </div>
             </article>
@@ -63,9 +63,9 @@
                     <img src="imgs/port_03-1.jpg" alt="Portraits">
                 </div>
                 <div class="album_info">
-                    <a href="gallery_portraits.php" class="category_button">Portraits</a>
+                    <a href="gallery/gallery_portraits.php" class="category_button">Portraits</a>
                     <h4 class="album_details">
-                        <a href="gallery_portraits.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                        <a href="gallery/gallery_portraits.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
                     </h4>
                 </div>
             </article>
@@ -74,9 +74,9 @@
                     <img src="imgs/sp_event_05-1.jpg" alt="Special Events">
                 </div>
                 <div class="album_info">
-                    <a href="gallery_special_event.php" class="category_button">Special Event</a>
+                    <a href="ggallery/allery_special_event.php" class="category_button">Special Event</a>
                     <h4 class="album_details">
-                        <a href="gallery_special_event.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                        <a href="gallery/gallery_special_event.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
                     </h4>
                 </div>
             </article>
@@ -85,9 +85,9 @@
                     <img src="imgs/baby_port_02-1.jpg" alt="Wild Life">
                 </div>
                 <div class="album_info">
-                    <a href="gallery_maternity.php" class="category_button">Baby Portraits</a>
+                    <a href="gallery/gallery_maternity.php" class="category_button">Baby Portraits</a>
                     <h4 class="album_details">
-                        <a href="gallery_maternity.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
+                        <a href="gallery/gallery_maternity.php">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima commodi soluta aut. Iusto distinctio vitae, </a>
                     </h4>
                 </div>
             </article>
