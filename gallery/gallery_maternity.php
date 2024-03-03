61af3c4ce3a841adc54e63bb42f179c20d975c94
Mohamed-Aseem <156054223+Mohamed-Aseem@users.noreply.github.com>
2024-03-03 21:56:28 +0530

    Created Maternity Gallery PHP File

---
+++ gallery/gallery_maternity.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include "header_gallery.php";
+    ?>
+</head>
+<body>
+    <nav>
+        <?php
+            include "nav_gallery.php";
+        ?>
+    </nav>
+
+    <div class="container wild_life_container">
+        <h3 class="album_title">Maternity / Baby Portraits</h3>
+        <div class="grid-wrapper">
+            
+            <?php
+                
+                include "../admin/dbConnection.php";
+                
+                $sql = "SELECT * FROM `photography` WHERE album = 'Maternity';";
+                $res = $con->query($sql);
+
+                if($res->num_rows>0){
+
+                    while($row= $res->fetch_assoc()){
+                        $classType = $row['type'];
+
+                        echo "  
+                            <div class='{$classType}'>
+                                <img src='{$row['image_url']}'>
+                            </div> 
+                        ";
+                    }
+
+                }
+                else{
+                    echo"   <div class='alert_message error'>
+                                <p>No Images Available In Database</p>
+                            </div>";
+                }
+
+            ?>
+
+        </div>
+    </div>
+
+
+    <script src="../js/main.js"></script>
+</body>
+</html>
