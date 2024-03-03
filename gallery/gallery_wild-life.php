7268fe3b7d9d490936c42c29f651b515d4fa1f1b
Mohamed-Aseem <156054223+Mohamed-Aseem@users.noreply.github.com>
2024-03-03 19:47:05 +0530

    Update gallery_wild-life.php with codes

--- gallery/gallery_wild-life.php
+++ gallery/gallery_wild-life.php
@@ -1,1 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include "header_gallery.php";
+    ?>
+</head>
+<body>
+    <nav>
+        <?php
+            include "nav_gallery.php";
+        ?>
+    </nav>
 
+    <div class="container wild_life_container">
+        <h3 class="album_title">Wide Life</h3>
+        <div class="grid-wrapper">
+            <?php
+                include "../admin/dbConnection.php";
+
+                $sql = "SELECT * FROM photography WHERE album = 'Wild Life'";
+                $res = $con->query($sql);
+                
+                if($res -> num_rows>0){
+                    while($row = $res->fatch_assoc()){
+                        $className = $row['type'];
+
+                        echo "
+                        <div class='{$className}'>
+                            <img src='{$row['image_url']}' />
+                        </div>
+                        ";
+                    }
+                    
+                }else{
+                    echo"   
+                        <div class='alert_message error'>
+                            <p>No Images Available In Database</p>
+                        </div>";
+                }
+                
+            ?>
+        </div>
+    </div>
+
+
+    <script src="../js/main.js"></script>
+</body>
+</html>
