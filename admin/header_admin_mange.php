8bea2fce7c73eaa3f920e67e6234cd88e16f2e28
Mohamed-Aseem <156054223+Mohamed-Aseem@users.noreply.github.com>
2024-03-07 22:13:35 +0530

    Created Header Admin Manage PHP File

---
+++ admin/header_admin_mange.php
@@ -0,0 +1,12 @@
+<!-- GOOGLE FONT (Montserrat) -->
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+<!-- Boostrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+
+
+<!--------------------------------------- Alert Box CDN --------------------------------------->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+<link rel="stylesheet" href="../css/style_admin-manage.css">
