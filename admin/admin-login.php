6b2202f34171dade789bb7756fa315ae08527e69
Mohamed-Aseem <156054223+Mohamed-Aseem@users.noreply.github.com>
2024-03-10 10:19:11 +0530

    Update admin-login.php with header icon

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -28,6 +28,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Malcolm Lismore</title>
     <link rel="stylesheet" href="../css/style_admin-login.css">
+    <link rel="shortcut icon" href="../imgs/logo.png" type="image/x-icon">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
